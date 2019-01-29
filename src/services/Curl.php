595342189a20fdca31b6a654115c5457a75387fd
juliengriffoul <julien.griffoul@viacesi.fr>
2019-01-29 19:52:54 +0100

    :pencil2: Add comments on Curl.php

--- src/services/Curl.php
+++ src/services/Curl.php
@@ -17,24 +17,33 @@ class Curl
      */
     public function faireRequete($method, $url, $data = false)
     {
-        $curlObject = curl_init();
-        curl_setopt($curlObject, CURLOPT_URL, $url);
+        $curlObject = curl_init(); // Initialise the curl request
+
+        curl_setopt($curlObject, CURLOPT_URL, $url); // Set URL option
+
         switch ($method) {
-            case 'POST':
-                curl_setopt($curlObject, CURLOPT_POST, 1);
-                if ($data)
+            case 'POST': // If this is a post request
+                curl_setopt($curlObject, CURLOPT_POST, 1); // Set option POST
+
+                if ($data) {
                     curl_setopt($curlObject, CURLOPT_POSTFIELDS, $data);
+                }
                 break;
+
             default:
                 curl_setopt($curlObject, CURLOPT_URL, $url);
                 break;
         }
-        curl_setopt($curlObject, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curlObject);
 
+        curl_setopt($curlObject, CURLOPT_RETURNTRANSFER, true); // Set another option
+        $response = curl_exec($curlObject); // Execute the request
+
+        // Handle curl response error
         if ($response === false) {
             echo 'Erreur Curl : ' . curl_error($curlObject);
         }
+
+        // Handle error status code
         if (!curl_errno($curlObject)) {
             switch ($http_code = curl_getinfo($curlObject, CURLINFO_HTTP_CODE)) {
                 case 200:
@@ -43,10 +52,11 @@ class Curl
                     echo 'Erreur HTTP: ', $http_code, "\n";
             }
         }
-        curl_close($curlObject);
-        //dump(curl_getinfo($curlObject, CURLINFO_HTTP_CODE));
-        $result[] = substr($response, 0);
-        return $result[0];
+
+        curl_close($curlObject); // Free the memory used by the request
+
+        $result[] = substr($response, 0); // Parse the JSON response
+        return $result[0]; // Return the response
     }
 
     /**
@@ -58,41 +68,48 @@ class Curl
      */
     public function faireRequeteAvecHeader($method, $url, $token, $data = false)
     {
-        $curlObject = curl_init();
+        $curlObject = curl_init(); // Initialise the curl request
 
-        curl_setopt($curlObject, CURLOPT_URL, $url);
+        curl_setopt($curlObject, CURLOPT_URL, $url); // Set URL option
 
+        // Define the header
         $header = array(
             "verificateur: e " . $token
         );
 
         switch ($method) {
-            case 'POST':
+            case 'POST': // POST requests
                 curl_setopt($curlObject, CURLOPT_POST, 1);
                 curl_setopt($curlObject, CURLOPT_HTTPHEADER, $header);
-                if ($data)
+
+                if ($data) {
                     curl_setopt($curlObject, CURLOPT_POSTFIELDS, $data);
+                }
                 break;
 
-            case 'DELETE':
-                if ($data) {
-                }
+            case 'DELETE': // DELETE requests
                 curl_setopt($curlObject, CURLOPT_CUSTOMREQUEST, "DELETE");
                 curl_setopt($curlObject, CURLOPT_HTTPHEADER, $header);
                 break;
 
-            default:
+            default: // GET and PUT requests
                 curl_setopt($curlObject, CURLOPT_URL, $url);
                 curl_setopt($curlObject, CURLOPT_HEADER, $header);
                 break;
         }
+
+        // Set different options
         curl_setopt($curlObject, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curlObject, CURLOPT_HEADER, false);
-        $response = curl_exec($curlObject);
 
+        $response = curl_exec($curlObject); // Execution of the curl request
+
+        // Handle curl response error
         if ($response === false) {
             echo 'Erreur Curl : ' . curl_error($curlObject);
         }
+
+        // Handle error status code
         if (!curl_errno($curlObject)) {
             switch ($http_code = curl_getinfo($curlObject, CURLINFO_HTTP_CODE)) {
                 case 200:
@@ -101,30 +118,42 @@ class Curl
                     echo 'Erreur HTTP: ', $http_code, "\n";
             }
         }
-        curl_close($curlObject);
-        //dump(curl_getinfo($curlObject, CURLINFO_HTTP_CODE));
-        $result[] = substr($response, 0);
-        return $result[0];
+
+        curl_close($curlObject); // Free the memory used by the request
+
+        $result[] = substr($response, 0); // Parse the JSON response
+        return $result[0]; // Return the response
     }
+
+    /**
+     * @param $method
+     * @param $url
+     * @param $token
+     * @param bool $data
+     * @param $path
+     * @param $type
+     * @return mixed
+     */
     public function faireRequeteAvecFichier($method, $url, $token, $data = false, $path, $type)
     {
-        $curlObject = curl_init();
+        $curlObject = curl_init(); // Initialise the curl request
+
+        // Set different options
         curl_setopt($curlObject, CURLOPT_URL, $url);
         curl_setopt($curlObject, CURLOPT_VERBOSE, 1);
+
         $header = array(
             "verificateur: e " . $token
         );
+
         switch ($method) {
             case 'POST':
                 curl_setopt($curlObject, CURLOPT_POST, 1);
-                //curl_setopt($curlObject, CURLOPT_HTTPHEADER, $header);
-                curl_setopt($curlObject, CURLOPT_HTTPHEADER, array(
-                    /*'X-Apple-Tz: 0',
-                    'X-Apple-Store-Front: 143444,12'*/
-                    "verificateur: e " . $token
-                ));
+                curl_setopt($curlObject, CURLOPT_HTTPHEADER, $header);
+
                 if ($data) {
-                    if ($type === 'produit') {
+
+                    if ($type === 'produit') { // POST request for a product
                         $arr = json_decode($data, true);
                         $b = $arr["nom_produit"];
                         \dump($arr["nom_produit"]);
@@ -140,25 +169,31 @@ class Curl
                             'formulaire' => $data
                         );
                     }
-                    //  $postfields["upfile"] = "$path";
                     curl_setopt($curlObject, CURLOPT_POSTFIELDS, $postfields);
                 }
                 break;
-            case 'DELETE':
+
+            case 'DELETE': // DELETE requests
                 if ($data)
                     curl_setopt($curlObject, CURLOPT_POSTFIELDS, $data);
                 break;
+
             default:
                 curl_setopt($curlObject, CURLOPT_URL, $url);
                 curl_setopt($curlObject, CURLOPT_HEADER, $header);
                 break;
         }
+
         curl_setopt($curlObject, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curlObject);
-        //dump(curl_getinfo($curlObject, CURLINFO_HTTP_CODE));
+
+        $response = curl_exec($curlObject); // Execute the request
+
+        // Handle curl error
         if ($response === false) {
             echo 'Erreur Curl : ' . curl_error($curlObject);
         }
+
+        // handle status code errors
         if (!curl_errno($curlObject)) {
             switch ($http_code = curl_getinfo($curlObject, CURLINFO_HTTP_CODE)) {
                 case 200:
@@ -167,7 +202,8 @@ class Curl
                     echo 'Erreur HTTP: ', $http_code, "\n";
             }
         }
-        $result[] = substr($response, 0);
-        return $result[0];
+
+        $result[] = substr($response, 0); // Parse the JSON response
+        return $result[0]; // Return the response
     }
 }
